2a769972e69de74eeae571009c96f9b744bb40ab
Iwan Setiyo <setiyo.iwan@gmail.com>
2022-07-08 15:56:37 +0700

    update git

--- .script/script.sh
+++ .script/script.sh
@@ -4,6 +4,6 @@ set -e
 echo "Deployment started ..."
 
 # Pull the latest version of the app
-git pull origin dev
+git pull git@github.com:iwanmj/test-github-action.git dev
 
 echo "Deployment finished!"
